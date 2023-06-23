71e346e5b987a1f65c20937ef162d41ff7b498e0
TessaFondeur <fondeurtessa.plume@gmail.com>
2023-06-23 14:20:58 +0200

    Mistake corrected from useMediaQuery, use React Hook useMediaQuery and not React responsive

--- frontend/src/components/corporateValues/CorporateValues.jsx
+++ frontend/src/components/corporateValues/CorporateValues.jsx
@@ -1,12 +1,13 @@
 import React from "react";
+import { useMediaQuery } from "@react-hook/media-query";
 import { FiUsers, FiRefreshCw, FiTrendingUp } from "react-icons/fi";
-import { useMediaQuery } from "react-responsive";
 import "./CorporateValues.scss";
 
 export default function CorporateValues() {
-  const match = useMediaQuery("only screen and (max-width: 450px)");
+  const matchMobile = useMediaQuery("only screen and (max-width: 450px)");
+  const matchDesktop = useMediaQuery("only screen and (min-width: 450px)");
 
-  if (match)
+  if (matchMobile)
     return (
       <div className="container-values-mobile">
         <div className="image-text-mobile">
@@ -70,46 +71,48 @@ export default function CorporateValues() {
         </div>
       </div>
     );
-  return (
-    <div className="container-values-desktop">
-      <div className="image-text-desktop">
-        <img src="./src/assets/proximite.jpg" alt="solidarity" />
-        <div className="presentation-text">
-          <h2>Notre proximité</h2>
-          <p>
-            L’expérience professionnelle est une chose. L’expérience de vie en
-            est une autre. Alors nos consultants prennent le temps de faire
-            connaissance avec chaque personne, pour comprendre le contexte, le
-            parcours, les envies et les projets.
-          </p>
-        </div>
-      </div>
 
-      <div className="image-text-desktop">
-        <div className="presentation-text-desktop">
-          <h2>Notre performance</h2>
-          <p>
-            Notre réseau est une force et nous y travaillons sans relâche. Notre
-            expérience nous permet d’identifier les vrais besoins d’une
-            entreprise et de ceux qui la rejoignent.
-          </p>
+  if (matchDesktop)
+    return (
+      <div className="container-values-desktop">
+        <div className="image-text-desktop">
+          <img src="./src/assets/proximite.jpg" alt="solidarity" />
+          <div className="presentation-text">
+            <h2>Notre proximité</h2>
+            <p>
+              L’expérience professionnelle est une chose. L’expérience de vie en
+              est une autre. Alors nos consultants prennent le temps de faire
+              connaissance avec chaque personne, pour comprendre le contexte, le
+              parcours, les envies et les projets.
+            </p>
+          </div>
         </div>
-        <img src="./src/assets/performance.jpg" alt="woman's working" />
-      </div>
 
-      <div className="image-text-desktop">
-        <img src="./src/assets/durable.jpg" alt="smile" />
-        <div className="presentation-text-desktop">
-          <h2>Notre durabilité</h2>
-          <p>
-            Notre challenge est de trouver l’équipe qui fonctionnera ensemble de
-            manière professionnelle et personnelle, pour aller jusqu’au bout
-            d’un projet commun. Notre responsabilité vis-à-vis des impacts de
-            nos décisions et nos actions sur le long-terme correspondent
-            également à notre politique RSE.
-          </p>
+        <div className="image-text-desktop">
+          <div className="presentation-text-desktop">
+            <h2>Notre performance</h2>
+            <p>
+              Notre réseau est une force et nous y travaillons sans relâche.
+              Notre expérience nous permet d’identifier les vrais besoins d’une
+              entreprise et de ceux qui la rejoignent.
+            </p>
+          </div>
+          <img src="./src/assets/performance.jpg" alt="woman's working" />
+        </div>
+
+        <div className="image-text-desktop">
+          <img src="./src/assets/durable.jpg" alt="smile" />
+          <div className="presentation-text-desktop">
+            <h2>Notre durabilité</h2>
+            <p>
+              Notre challenge est de trouver l’équipe qui fonctionnera ensemble
+              de manière professionnelle et personnelle, pour aller jusqu’au
+              bout d’un projet commun. Notre responsabilité vis-à-vis des
+              impacts de nos décisions et nos actions sur le long-terme
+              correspondent également à notre politique RSE.
+            </p>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
 }
