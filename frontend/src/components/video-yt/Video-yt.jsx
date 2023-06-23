5dee58fb432c4094af9d1d90644ace36f04223d4
TessaFondeur <fondeurtessa.plume@gmail.com>
2023-06-23 09:30:50 +0200

    Component Video-yt, Done !

--- frontend/src/components/video-yt/Video-yt.jsx
+++ frontend/src/components/video-yt/Video-yt.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactPlayer from "react-player";
 import { useMediaQuery } from "@react-hook/media-query";
+import "./Video-yt.scss";
 
 export default function Video() {
   const match = useMediaQuery("only screen and (min-width: 450px)");
@@ -10,6 +11,9 @@ export default function Video() {
   if (match)
     return (
       <div className="video-player">
+        <h1 className="title">
+          À propos <span className="title-bold">de nous</span>
+        </h1>
         <ReactPlayer url={externaticVideo} />
       </div>
     );
