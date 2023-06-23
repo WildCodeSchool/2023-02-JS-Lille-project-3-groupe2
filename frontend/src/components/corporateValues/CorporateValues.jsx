ba4a5c48285a00a4ddd7ab6a414665ef676fc763
TessaFondeur <fondeurtessa.plume@gmail.com>
2023-06-23 17:21:53 +0200

    Component for corporate values, Done for MOBILE and DESKTOP

--- frontend/src/components/corporateValues/CorporateValues.jsx
+++ frontend/src/components/corporateValues/CorporateValues.jsx
@@ -76,10 +76,17 @@ export default function CorporateValues() {
     return (
       <div className="container-values-desktop">
         <div className="image-text-desktop">
-          <img src="./src/assets/proximite.jpg" alt="solidarity" />
-          <div className="presentation-text">
-            <h2>Notre proximité</h2>
-            <p>
+          <img
+            className="img-values-desktop"
+            src="./src/assets/proximite.jpg"
+            alt="solidarity"
+          />
+          <div className="presentation-text-desktop">
+            <div className="title-values-desktop">
+              <h2>Notre proximité</h2>
+              <FiUsers className="icon-react-desktop" />
+            </div>
+            <p className="p-desktop">
               L’expérience professionnelle est une chose. L’expérience de vie en
               est une autre. Alors nos consultants prennent le temps de faire
               connaissance avec chaque personne, pour comprendre le contexte, le
@@ -90,21 +97,35 @@ export default function CorporateValues() {
 
         <div className="image-text-desktop">
           <div className="presentation-text-desktop">
-            <h2>Notre performance</h2>
-            <p>
+            <div className="title-values-desktop">
+              <h2>Notre performance</h2>
+              <FiTrendingUp className="icon-react-desktop" />
+            </div>
+            <p className="p-desktop">
               Notre réseau est une force et nous y travaillons sans relâche.
               Notre expérience nous permet d’identifier les vrais besoins d’une
               entreprise et de ceux qui la rejoignent.
             </p>
           </div>
-          <img src="./src/assets/performance.jpg" alt="woman's working" />
+          <img
+            className="img-values-desktop"
+            src="./src/assets/performance.jpg"
+            alt="woman's working"
+          />
         </div>
 
         <div className="image-text-desktop">
-          <img src="./src/assets/durable.jpg" alt="smile" />
+          <img
+            className="img-values-desktop"
+            src="./src/assets/durable.jpg"
+            alt="smile"
+          />
           <div className="presentation-text-desktop">
-            <h2>Notre durabilité</h2>
-            <p>
+            <div className="title-values-desktop">
+              <h2>Notre durabilité</h2>
+              <FiRefreshCw className="icon-react-desktop" />
+            </div>
+            <p className="p-desktop">
               Notre challenge est de trouver l’équipe qui fonctionnera ensemble
               de manière professionnelle et personnelle, pour aller jusqu’au
               bout d’un projet commun. Notre responsabilité vis-à-vis des
