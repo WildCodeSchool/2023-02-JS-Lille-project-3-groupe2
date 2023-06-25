65b85e06bf587527208951725eb412851b9df99b
ImeneRdn <imene.ramdan@live.fr>
2023-06-25 18:40:29 +0200

    update slider automatic partner

--- frontend/src/components/sliderAutomatic/SliderAutomatic.jsx
+++ frontend/src/components/sliderAutomatic/SliderAutomatic.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import automotiveCells from "../../assets/automotiveCells.png";
 import iadvize from "../../assets/iadvize.png";
 import iris from "../../assets/iris.png";
@@ -10,46 +10,57 @@ import maisondumonde from "../../assets/maisondumonde.png";
 import klaxoon from "../../assets/klaxoon.png";
 import groupama from "../../assets/groupama.png";
 import lumiplan from "../../assets/lumiplan.png";
+import akeneo from "../../assets/akeneo.png";
+import allovoisin from "../../assets/allovoisin.png";
+import lucca from "../../assets/lucca.png";
+import nickel from "../../assets/nickel.png";
+import showroomprive from "../../assets/showroomprive.png";
 import "./SliderAutomatic.scss";
 
-export default function SliderAutomatic() {
+function SliderAutomatic() {
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const logos = [
+    lucca,
+    akeneo,
+    allovoisin,
+    nickel,
+    iris,
+    maisondumonde,
+    showroomprive,
+    manitou,
+    iadvize,
+    ikks,
+    lengow,
+    maincair,
+    automotiveCells,
+    klaxoon,
+    groupama,
+    lumiplan,
+  ];
+  const logosLoop = [...logos, ...logos];
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentSlide((prevSlide) => (prevSlide + 1) % logos.length);
+    }, 3000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [logos.length]);
   return (
     <div className="slider">
-      <div className="slide-track">
-        <div className="slide">
-          <img src={iadvize} alt="logo-iadvize" />
-        </div>
-        <div className="slide">
-          <img src={automotiveCells} alt="" />
-        </div>
-        <div className="slide">
-          <img src={ikks} alt="" />
-        </div>
-        <div className="slide">
-          <img src={iris} alt="" />
-        </div>
-        <div className="slide">
-          <img src={lengow} alt="" />
-        </div>
-        <div className="slide">
-          <img src={maincair} alt="" />
-        </div>
-        <div className="slide">
-          <img src={manitou} alt="" />
-        </div>
-        <div className="slide">
-          <img src={maisondumonde} alt="" />
-        </div>
-        <div className="slide">
-          <img src={klaxoon} alt="" />
-        </div>
-        <div className="slide">
-          <img src={groupama} alt="" />
-        </div>
-        <div className="slide">
-          <img src={lumiplan} alt="" />
-        </div>
+      <div
+        className="slide-track"
+        style={{ transform: `translateX(-${currentSlide * 100}%)` }}
+      >
+        {logosLoop.map((logo, index) => (
+          <div className={`slide ${index === currentSlide ? "active" : ""}`}>
+            <img className="img-partner" src={logo} alt="logo-partner" />
+          </div>
+        ))}
       </div>
     </div>
   );
 }
+export default React.memo(SliderAutomatic);
