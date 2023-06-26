345922bf7a2bf001133bb5a6a5583596794aea8b
TessaFondeur <fondeurtessa.plume@gmail.com>
2023-06-26 09:42:55 +0200

    Component for Corporate Values, CSS and proptypes for switch color, Done !

--- frontend/src/components/corporateValues/CorporateValues.jsx
+++ frontend/src/components/corporateValues/CorporateValues.jsx
@@ -1,72 +1,79 @@
 import React from "react";
 import { useMediaQuery } from "@react-hook/media-query";
 import { FiUsers, FiRefreshCw, FiTrendingUp } from "react-icons/fi";
+import PropTypes from "prop-types";
 import "./CorporateValues.scss";
 
-export default function CorporateValues() {
+export default function CorporateValues({ role }) {
   const matchMobile = useMediaQuery("only screen and (max-width: 750px)");
   const matchDesktop = useMediaQuery("only screen and (min-width: 750px)");
 
   if (matchMobile)
     return (
-      <div className="container-values-mobile">
-        <div className="image-text-mobile">
-          <img
-            className="img-values-mobile"
-            src="./src/assets/proximite.jpg"
-            alt="solidarity"
-          />
-          <div className="presentation-text-mobile">
-            <div className="title-values-mobile">
-              <h2>Notre proximité</h2>
-              <FiUsers className="icon-react-mobile" />
+      <div
+        className={
+          role === "default" ? "" : role === "enterprise" && "role_enterprise"
+        }
+      >
+        <div className="container-values-mobile">
+          <div className="image-text-mobile">
+            <img
+              className="img-values-mobile"
+              src="./src/assets/proximite.jpg"
+              alt="solidarity"
+            />
+            <div className="presentation-text-mobile">
+              <div className="title-values-mobile">
+                <h2>Notre proximité</h2>
+                <FiUsers className="icon-react-mobile" />
+              </div>
+              <p>
+                L’expérience professionnelle est une chose. L’expérience de vie
+                en est une autre. Alors nos consultants prennent le temps de
+                faire connaissance avec chaque personne, pour comprendre le
+                contexte, le parcours, les envies et les projets.
+              </p>
             </div>
-            <p>
-              L’expérience professionnelle est une chose. L’expérience de vie en
-              est une autre. Alors nos consultants prennent le temps de faire
-              connaissance avec chaque personne, pour comprendre le contexte, le
-              parcours, les envies et les projets.
-            </p>
           </div>
-        </div>
 
-        <div className="image-text-mobile">
-          <img
-            className="img-values-mobile"
-            src="./src/assets/performance.jpg"
-            alt="woman's working"
-          />
-          <div className="presentation-text-mobile">
-            <div className="title-values-mobile">
-              <h2>Notre performance</h2>
-              <FiTrendingUp className="icon-react-mobile" />
+          <div className="image-text-mobile">
+            <img
+              className="img-values-mobile"
+              src="./src/assets/performance.jpg"
+              alt="woman's working"
+            />
+            <div className="presentation-text-mobile">
+              <div className="title-values-mobile">
+                <h2>Notre performance</h2>
+                <FiTrendingUp className="icon-react-mobile" />
+              </div>
+              <p>
+                Notre réseau est une force et nous y travaillons sans relâche.
+                Notre expérience nous permet d’identifier les vrais besoins
+                d’une entreprise et de ceux qui la rejoignent.
+              </p>
             </div>
-            <p>
-              Notre réseau est une force et nous y travaillons sans relâche.
-              Notre expérience nous permet d’identifier les vrais besoins d’une
-              entreprise et de ceux qui la rejoignent.
-            </p>
           </div>
-        </div>
 
-        <div className="image-text-mobile">
-          <img
-            className="img-values-mobile"
-            src="./src/assets/durable.jpg"
-            alt="smile"
-          />
-          <div className="presentation-text-mobile">
-            <div className="title-values-mobile">
-              <h2>Notre durabilité</h2>
-              <FiRefreshCw className="icon-react-mobile" />
+          <div className="image-text-mobile">
+            <img
+              className="img-values-mobile"
+              src="./src/assets/durable.jpg"
+              alt="smile"
+            />
+            <div className="presentation-text-mobile">
+              <div className="title-values-mobile">
+                <h2>Notre durabilité</h2>
+                <FiRefreshCw className="icon-react-mobile" />
+              </div>
+              <p>
+                Notre challenge est de trouver l’équipe qui fonctionnera
+                ensemble de manière professionnelle et personnelle, pour aller
+                jusqu’au bout d’un projet commun. Notre responsabilité vis-à-vis
+                des impacts de nos décisions et nos actions sur le long-terme
+                correspondent également à notre politique RSE.
+              </p>
             </div>
-            <p>
-              Notre challenge est de trouver l’équipe qui fonctionnera ensemble
-              de manière professionnelle et personnelle, pour aller jusqu’au
-              bout d’un projet commun. Notre responsabilité vis-à-vis des
-              impacts de nos décisions et nos actions sur le long-terme
-              correspondent également à notre politique RSE.
-            </p>
           </div>
         </div>
       </div>
@@ -74,66 +81,76 @@ export default function CorporateValues() {
 
   if (matchDesktop)
     return (
-      <div className="container-values-desktop">
-        <div className="image-text-desktop">
-          <img
-            className="img-values-desktop"
-            src="./src/assets/proximite.jpg"
-            alt="solidarity"
-          />
-          <div className="presentation-text-desktop">
-            <div className="title-values-desktop">
-              <h2>Notre proximité</h2>
-              <FiUsers className="icon-react-desktop" />
+      <div
+        className={
+          role === "default" ? "" : role === "enterprise" && "role_enterprise"
+        }
+      >
+        <div className="container-values-desktop">
+          <div className="image-text-desktop">
+            <img
+              className="img-values-desktop"
+              src="./src/assets/proximite.jpg"
+              alt="solidarity"
+            />
+            <div className="presentation-text-desktop">
+              <div className="title-values-desktop">
+                <h2>Notre proximité</h2>
+                <FiUsers className="icon-react-desktop" />
+              </div>
+              <p className="p-desktop">
+                L’expérience professionnelle est une chose. L’expérience de vie
+                en est une autre. Alors nos consultants prennent le temps de
+                faire connaissance avec chaque personne, pour comprendre le
+                contexte, le parcours, les envies et les projets.
+              </p>
             </div>
-            <p className="p-desktop">
-              L’expérience professionnelle est une chose. L’expérience de vie en
-              est une autre. Alors nos consultants prennent le temps de faire
-              connaissance avec chaque personne, pour comprendre le contexte, le
-              parcours, les envies et les projets.
-            </p>
           </div>
-        </div>
 
-        <div className="image-text-desktop">
-          <div className="presentation-text-desktop">
-            <div className="title-values-desktop">
-              <h2>Notre performance</h2>
-              <FiTrendingUp className="icon-react-desktop" />
+          <div className="image-text-desktop">
+            <div className="presentation-text-desktop">
+              <div className="title-values-desktop">
+                <h2>Notre performance</h2>
+                <FiTrendingUp className="icon-react-desktop" />
+              </div>
+              <p className="p-desktop">
+                Notre réseau est une force et nous y travaillons sans relâche.
+                Notre expérience nous permet d’identifier les vrais besoins
+                d’une entreprise et de ceux qui la rejoignent.
+              </p>
             </div>
-            <p className="p-desktop">
-              Notre réseau est une force et nous y travaillons sans relâche.
-              Notre expérience nous permet d’identifier les vrais besoins d’une
-              entreprise et de ceux qui la rejoignent.
-            </p>
+            <img
+              className="img-values-desktop"
+              src="./src/assets/performance.jpg"
+              alt="woman's working"
+            />
           </div>
-          <img
-            className="img-values-desktop"
-            src="./src/assets/performance.jpg"
-            alt="woman's working"
-          />
-        </div>
 
-        <div className="image-text-desktop">
-          <img
-            className="img-values-desktop"
-            src="./src/assets/durable.jpg"
-            alt="smile"
-          />
-          <div className="presentation-text-desktop">
-            <div className="title-values-desktop">
-              <h2>Notre durabilité</h2>
-              <FiRefreshCw className="icon-react-desktop" />
+          <div className="image-text-desktop">
+            <img
+              className="img-values-desktop"
+              src="./src/assets/durable.jpg"
+              alt="smile"
+            />
+            <div className="presentation-text-desktop">
+              <div className="title-values-desktop">
+                <h2>Notre durabilité</h2>
+                <FiRefreshCw className="icon-react-desktop" />
+              </div>
+              <p className="p-desktop">
+                Notre challenge est de trouver l’équipe qui fonctionnera
+                ensemble de manière professionnelle et personnelle, pour aller
+                jusqu’au bout d’un projet commun. Notre responsabilité vis-à-vis
+                des impacts de nos décisions et nos actions sur le long-terme
+                correspondent également à notre politique RSE.
+              </p>
             </div>
-            <p className="p-desktop">
-              Notre challenge est de trouver l’équipe qui fonctionnera ensemble
-              de manière professionnelle et personnelle, pour aller jusqu’au
-              bout d’un projet commun. Notre responsabilité vis-à-vis des
-              impacts de nos décisions et nos actions sur le long-terme
-              correspondent également à notre politique RSE.
-            </p>
           </div>
         </div>
       </div>
     );
 }
+
+CorporateValues.propTypes = {
+  role: PropTypes.string.isRequired,
+};
