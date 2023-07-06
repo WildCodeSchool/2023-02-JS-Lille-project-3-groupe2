76bebf347b0c20fb2e885c69c24abb857988d340
TessaFondeur <fondeurtessa.plume@gmail.com>
2023-07-06 14:18:26 +0200

    continued

--- frontend/src/components/cardprofil/ProfilCard.jsx
+++ frontend/src/components/cardprofil/ProfilCard.jsx
@@ -1,3 +1,47 @@
 import "./ProfilCard.scss";
+import { FiEdit } from "react-icons/fi";
 
-export default function ProfilCard() {}
+export default function ProfilCard() {
+  return (
+    <div className="profil-card-container">
+      <div className="container-left-profil">
+        <img
+          src="./src/assets/portrait-homme-riant_23-2148859448.jpg"
+          alt="profil"
+        />
+      </div>
+      <div className="container-right-profil">
+        <h2>
+          Bonjour <span>Clément</span> !
+        </h2>
+        <div className="item-profil">
+          <h3>
+            <li>Je suis à la recherche de :</li>
+          </h3>
+          <p>CDI, CDD, Interim ...</p>
+        </div>
+        <div className="item-profil">
+          <h3>
+            <li>En tant que :</li>
+          </h3>
+          <p>Développeur, Manager ...</p>
+        </div>
+        <div className="item-profil">
+          <h3>
+            <li>Lieu souhaité :</li>
+          </h3>
+          <p>Haut-de-France</p>
+        </div>
+        <div className="item-profil">
+          <h3>
+            <li>Disponibilité :</li>
+          </h3>
+          <p>22.02.2024</p>
+        </div>
+      </div>
+      <div className="button-container-modal">
+        <FiEdit />
+      </div>
+    </div>
+  );
+}
--- frontend/src/pages/default/DefaultHome.jsx
+++ frontend/src/pages/default/DefaultHome.jsx
@@ -1,1 +1,5 @@
-export default function DefaultHome() {}
+import ProfilCard from "../../components/cardprofil/ProfilCard";
+
+export default function DefaultHome() {
+  return <ProfilCard />;
+}
