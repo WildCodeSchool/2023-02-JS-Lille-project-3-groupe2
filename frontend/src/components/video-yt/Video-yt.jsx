fdc2e90946b9a132ea9fb54949175e2eda9539e8
SachaWildCode <sacha.loumachi@gmail.com>
2023-07-07 14:51:46 +0200

    video fix

--- frontend/src/components/video-yt/Video-yt.jsx
+++ frontend/src/components/video-yt/Video-yt.jsx
@@ -4,17 +4,17 @@ import { useMediaQuery } from "@react-hook/media-query";
 import "./Video-yt.scss";
 
 export default function Video() {
-  const match = useMediaQuery("only screen and (min-width: 450px)");
+  const match = useMediaQuery("only screen and (min-width: 650px)");
   const externaticVideo =
     "https://www.youtube.com/watch?v=BYmNVsS5J58&ab_channel=Externatic-Cabinetderecrutementinformatique";
 
   if (match)
     return (
-      <div className="video-player">
+      <div className="video-wrapper">
         <h1 className="title">
           À propos <span className="title-bold">de nous</span>
         </h1>
-        <ReactPlayer url={externaticVideo} />
+        <ReactPlayer className="video-player" url={externaticVideo} />
       </div>
     );
 }
