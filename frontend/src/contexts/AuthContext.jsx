1a8f0c0c089dd89bbea955e5a3bee548d0bfc79e
SachaWildCode <sacha.loumachi@gmail.com>
2023-07-16 17:54:31 +0200

    useAuth fix

--- frontend/src/contexts/AuthContext.jsx
+++ frontend/src/contexts/AuthContext.jsx
@@ -1,32 +1,51 @@
-import { createContext, useContext, useEffect, useState } from "react";
+import { createContext, useContext, useMemo, useState } from "react";
 import propTypes from "prop-types";
-import api from "../services/api";
+// import api from "../services/api";
 
 const authContext = createContext();
 
 export function AuthProvider({ children }) {
   const { Provider } = authContext;
+  /* eslint-disable */
+  const [user, setUser] = useState({
+    auth: null,
+    infos: null,
+  });
 
-  const [userAccount, setUserAccount] = useState();
-  const [userInfos, setUserInfos] = useState();
+  const isLogged = () => {
+    console.log(user);
+    return user.auth && user.infos ? true : false;
+  };
 
-  const Login = async () => {
+  const Login = async (email, password) => {
     /* Axios get here then set user Account and user Infos */
     try {
-      const result = await api.get("/login");
-      console.info(result);
+      //   const result = await api.get("/login");
+      //   console.info(result);
+      // set user here
     } catch (err) {
       console.error(err);
     }
   };
 
-  useEffect(() => {
-    Login();
-  }, []);
-
-  return (
-    <Provider value={{ userAccount, Login, userInfos }}>{children}</Provider>
+  const Logout = () => {
+    setUser({
+      auth: null,
+      infos: null,
+    });
+  };
+  const value = useMemo(
+    () => ({
+      user,
+      setUser,
+      Login,
+      Logout,
+      isLogged,
+    }),
+    [user, setUser, Login, Logout, isLogged]
   );
+
+  return <Provider value={value}>{children}</Provider>;
 }
 
 AuthProvider.propTypes = {
