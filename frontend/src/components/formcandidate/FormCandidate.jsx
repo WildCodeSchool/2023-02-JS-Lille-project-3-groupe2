e46490a4d0288e34ee6c7f3922b0045ad5be6eb6
TessaFondeur <fondeurtessa.plume@gmail.com>
2023-07-17 17:25:22 +0200

    test for a register slider

---
+++ frontend/src/components/formcandidate/FormCandidate.jsx
@@ -0,0 +1,139 @@
+import "./FormCandidate.scss";
+
+export default function FormCandidate() {
+  return (
+    <div className="container-register-candidate">
+      <form className="form-candidate-container">
+        <div className="mail-password-container">
+          <h2>bouh</h2>
+          <div className="all-first-input">
+            <div className="mail-input">
+              <label>
+                <input />
+              </label>
+            </div>
+            <div className="-password-input">
+              <label>
+                <input />
+              </label>
+            </div>
+            <div className="confirm-password-input">
+              <label>
+                <input />
+              </label>
+            </div>
+          </div>
+        </div>
+
+        <div className="name-birthday-container">
+          <h2>bouh</h2>
+          <div className="all-second-input">
+            <div className="firstname-input">
+              <label>
+                <input />
+              </label>
+            </div>
+            <div className="lastname-input">
+              <label>
+                <input />
+              </label>
+            </div>
+            <div className="birthday-input">
+              <label>
+                <input />
+              </label>
+            </div>
+          </div>
+        </div>
+
+        <div className="address-container">
+          <h2>bouh</h2>
+          <div className="all-fourth-input">
+            <div className="number-way-input">
+              <label>
+                <input />
+              </label>
+            </div>
+            <div className="type-way-input">
+              <label>
+                <input />
+              </label>
+            </div>
+            <div className="name-way-input">
+              <label>
+                <input />
+              </label>
+            </div>
+            <div className="region-input">
+              <label>
+                <input />
+              </label>
+            </div>
+            <div className="department-input">
+              <label>
+                <input />
+              </label>
+            </div>
+            <div className="city-input">
+              <label>
+                <input />
+              </label>
+            </div>
+            <div className="postal-code-input">
+              <label>
+                <input />
+              </label>
+            </div>
+          </div>
+        </div>
+
+        <div className="contract-check-container">
+          <h2>bouh</h2>
+          <div className="all-fifth-input">
+            <div className="cdi-input">
+              <label>
+                <input />
+              </label>
+            </div>
+            <div className="cdd-input">
+              <label>
+                <input />
+              </label>
+            </div>
+            <div className="interim-input">
+              <label>
+                <input />
+              </label>
+            </div>
+            <div className="alternance-input">
+              <label>
+                <input />
+              </label>
+            </div>
+            <div className="stage-input">
+              <label>
+                <input />
+              </label>
+            </div>
+            <div className="other-input">
+              <label>
+                <input />
+              </label>
+            </div>
+          </div>
+        </div>
+
+        <div className="rgpd-check-container">
+          <h2>bouh</h2>
+          <div className="sixth-input">
+            <div className="rgpd-input">
+              <label>
+                <input />
+              </label>
+            </div>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+}
