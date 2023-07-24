82b78a8766b530465f25d4a529ea78675af2a96d
TessaFondeur <fondeurtessa.plume@gmail.com>
2023-07-24 11:44:01 +0200

    Modify Section, done ! Missing back.

--- frontend/src/components/modifysection/ModifySection.jsx
+++ frontend/src/components/modifysection/ModifySection.jsx
@@ -10,107 +10,109 @@ export default function ModifySection() {
 
   const sectionContents = {
     "Vos Identifiants": (
-      <div>
-        <h2>Vos identifiants</h2>
-        <p>Email actuel : ...</p>
-        <label htmlFor="email">Nouvel email :</label>
-        <input id="email" type="email" placeholder="johndoe@exemple.com ..." />
-        <label htmlFor="password">Votre mot de passe actuel :</label>
-        <input id="password" type="password" />
-        <label htmlFor="newpassword">Nouveau mot de passe :</label>
-        <input id="newpassword" type="password" />
-        <label htmlFor="newpassword">Confirmer mot de passe :</label>
-        <input id="newpassword" type="password" />
+      <div className="auth-section">
+        <h1>Vos identifiants</h1>
+        <div className="container-auth-section">
+          <label htmlFor="email">Email :</label>
+          <input
+            id="email"
+            type="email"
+            placeholder="johndoe@exemple.com ..."
+          />
+          <label htmlFor="password">Votre mot de passe actuel :</label>
+          <input id="password" type="password" />
+          <label htmlFor="newpassword">Nouveau mot de passe :</label>
+          <input id="newpassword" type="password" />
+          <label htmlFor="newpassword">Confirmer mot de passe :</label>
+          <input id="newpassword" type="password" />
+        </div>
       </div>
     ),
     "Votre Profil": (
-      <div>
-        <h2>Votre profil</h2>
-        <label htmlFor="picture">Image de profil :</label>
-        <input id="picture" type="file" />
-        <label htmlFor="firstname">Prénom :</label>
-        <input id="firstname" type="text" placeholder="John ..." />
-        <label htmlFor="lastname">Nom :</label>
-        <input id="lastname" type="text" placeholder="Doe ..." />
-        <label htmlFor="birthdate">Date de naissance :</label>
-        <input
-          id="birthdate"
-          type="date"
-          min="1900-01-01"
-          max="2005-07-27"
-          pattern="\d{4}-\d{2}-\d{2}"
-        />
-        <label htmlFor="about">A propos de vous :</label>
-        <input
-          id="about"
-          type="text"
-          placeholder="Dites-nous en plus sur vous ! ..."
-        />
-        <label htmlFor="phonenumber">Téléphone :</label>
-        <input id="phonenumber" type="number" placeholder="0606060606 ..." />
+      <div className="profile-section">
+        <h1>Votre profil</h1>
+        <div className="container-all-profile">
+          <div className="left-container-profile">
+            <label htmlFor="picture">Image de profil :</label>
+            <input id="picture" type="file" />
+            <label htmlFor="lastname">Nom :</label>
+            <input id="lastname" type="text" placeholder="Doe ..." />
+            <label htmlFor="firstname">Prénom :</label>
+            <input id="firstname" type="text" placeholder="John ..." />
+          </div>
+          <div className="right-container-profile">
+            <label htmlFor="birthdate">Date de naissance :</label>
+            <input
+              id="birthdate"
+              type="date"
+              min="1900-01-01"
+              max="2005-07-27"
+              pattern="\d{4}-\d{2}-\d{2}"
+            />
+            <label htmlFor="phonenumber">Téléphone :</label>
+            <input
+              id="phonenumber"
+              type="number"
+              placeholder="0606060606 ..."
+            />
+            <label htmlFor="about">A propos de vous :</label>
+            <input
+              id="about"
+              type="text"
+              placeholder="Dites-nous en plus sur vous ! ..."
+            />
+          </div>
+        </div>
       </div>
     ),
     "Votre Adresse": (
-      <div>
-        <h2>Votre adresse :</h2>
-        <label htmlFor="numberway">N° de voirie :</label>
-        <input id="numberway" type="number" placeholder="32 ..." />
-        <label htmlFor="typeway">Type de voirie :</label>
-        <input id="typeway" type="text" placeholder="Rue / Boulevard ..." />
-        <label htmlFor="nameway">Nom de voirie :</label>
-        <input id="nameway" type="text" placeholder="De la mouette ..." />
-        <label htmlFor="postalcode">Code Postal :</label>
-        <input id="postalcode" type="number" placeholder="59000 ..." />
-        <label htmlFor="city">Ville :</label>
-        <input id="city" type="text" placeholder="Lille ..." />
-        <label htmlFor="region">Région :</label>
-        <input id="region" type="text" placeholder="Haut-de-France ..." />
-        <label htmlFor="department">Département :</label>
-        <input id="department" type="text" placeholder="Nord ..." />
-        <label htmlFor="country">Pays :</label>
-        <input id="country" type="text" placeholder="France ..." />
-      </div>
-    ),
-    "Vos Recherches Emploi": (
-      <div>
-        <h2>Vos recherches d'emploi</h2>
-        <h3>Type de contrat :</h3>
-        <input id="cdi" type="checkbox" />
-        <label htmlFor="cdi">CDI</label>
-        <input id="cdd" type="checkbox" />
-        <label htmlFor="cdd">CDD</label>
-        <input id="alternance" type="checkbox" />
-        <label htmlFor="alternance">ALTERNANCE</label>
-        <input id="stage" type="checkbox" />
-        <label htmlFor="stage">STAGE</label>
-        <h3>Métier recherché :</h3>
-        <input id="dev" type="checkbox" />
-        <label htmlFor="dev">Développeur Web</label>
-        <input id="po" type="checkbox" />
-        <label htmlFor="po">Product Owner</label>
-        <input id="sm" type="checkbox" />
-        <label htmlFor="sm">Scrum Master</label>
-        <input id="tech" type="checkbox" />
-        <label htmlFor="tech">Tech Lead</label>
-        <h3>Où ?</h3>
-        <label htmlFor="cityjob">Ville de recherche :</label>
-        <input id="cityjob" type="text" />
+      <div className="address-section">
+        <h1>Votre adresse :</h1>
+        <div className="container-address-all">
+          <div className="left-container-address">
+            <label htmlFor="numberway">N° de voirie :</label>
+            <input id="numberway" type="number" placeholder="32 ..." />
+            <label htmlFor="typeway">Type de voirie :</label>
+            <input id="typeway" type="text" placeholder="Rue / Boulevard ..." />
+            <label htmlFor="nameway">Nom de voirie :</label>
+            <input id="nameway" type="text" placeholder="De la mouette ..." />
+            <label htmlFor="postalcode">Code Postal :</label>
+            <input id="postalcode" type="number" placeholder="59000 ..." />
+          </div>
+          <div className="right-container-address">
+            <label htmlFor="city">Ville :</label>
+            <input id="city" type="text" placeholder="Lille ..." />
+            <label htmlFor="region">Région :</label>
+            <input id="region" type="text" placeholder="Haut-de-France ..." />
+            <label htmlFor="department">Département :</label>
+            <input id="department" type="text" placeholder="Nord ..." />
+            <label htmlFor="country">Pays :</label>
+            <input id="country" type="text" placeholder="France ..." />
+          </div>
+        </div>
       </div>
     ),
   };
 
   return (
-    <div>
-      <h2>Que souhaitez-vous modifier ?</h2>
-      <select value={selectedSection} onChange={handleSectionChange}>
-        <option value="">Choisissez une section à modifier :</option>
-        <option value="Vos Identifiants">Vos Identifiants</option>
-        <option value="Votre Profil">Votre Profil</option>
-        <option value="Votre Adresse">Votre Adresse</option>
-        <option value="Vos Recherches Emploi">Vos Recherches d'emploi</option>
-      </select>
-      {sectionContents[selectedSection]}
-      {selectedSection && <p>Vous êtes dans : {selectedSection}</p>}
+    <div className="container-selection">
+      <h1>Que souhaitez-vous modifier ?</h1>
+      <div>
+        <select
+          className="options-selection"
+          value={selectedSection}
+          onChange={handleSectionChange}
+        >
+          <option value="">Choisissez une section à modifier</option>
+          <option value="Vos Identifiants">Vos Identifiants</option>
+          <option value="Votre Profil">Votre Profil</option>
+          <option value="Votre Adresse">Votre Adresse</option>
+        </select>
+      </div>
+      <div className="section-part-container">
+        {sectionContents[selectedSection]}
+      </div>
+      <div>{selectedSection && <p>Vous êtes dans : {selectedSection}</p>}</div>
     </div>
   );
 }
--- frontend/src/pages/registerpage/RegisterPage.jsx
+++ frontend/src/pages/registerpage/RegisterPage.jsx
@@ -1,7 +1,1 @@
-// import RegisterForm from "../../components/registerform/RegisterForm";
-
-import ModifySection from "../../components/modifysection/ModifySection";
-
-export default function RegisterPage() {
-  return <ModifySection />;
-}
+export default function RegisterPage() {}
